45fc872cfaa16db92e13278e8cc2fcc5e410d7e3
jongjin1010 <jongjin024@gmail.com>
2022-08-11 19:52:44 +0900

    create ApiForm

---
+++ src/Utils/basicApiForm.ts
@@ -0,0 +1,30 @@
+import { BASE_URL } from '../shared/config';
+import axios, { AxiosRequestConfig } from 'axios';
+
+const basicApiForm = (userData: AxiosRequestConfig, token?: string) => {
+  try {
+    const res = token
+      ? axios({
+          method: userData.method,
+          baseURL: BASE_URL,
+          url: userData.url,
+          data: userData.data,
+          withCredentials: true,
+          headers: {
+            cookie: token,
+          },
+        })
+      : axios({
+          method: userData.method,
+          baseURL: BASE_URL,
+          url: userData.url,
+          data: userData.data,
+          withCredentials: true,
+        });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default basicApiForm;
