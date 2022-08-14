99408db0a42f9b8c4f473a39f2235fdeb3d318a7
jongjin <jongjin024@gmail.com>
2022-08-15 02:37:43 +0900

    이유를 모르겠어요

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://paperserver-env.eba-6yj4r5c7.ap-northeast-2.elasticbeanstalk.com';
+export const BASE_URL =
+  "http://paperserver-env.eba-6yj4r5c7.ap-northeast-2.elasticbeanstalk.com";
