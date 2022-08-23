fc51e323ee5c4cc5e6ff5e24830da886e462269c
GSM06 <gsm06@GSM06ui-MacBookPro.local>
2022-08-23 20:30:45 +0900

    Add: 게시물박스

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,16 @@
+import * as S from "./style";
+
+const BoardPage: React.FC = () => {
+  return (
+    <>
+      <S.Container>
+        <S.Box>
+          <S.Title>ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅁㅈㅇㅈㅇ</S.Title>
+          <S.Date>2022-07-16</S.Date>
+        </S.Box>
+      </S.Container>
+    </>
+  );
+};
+
+export default BoardPage;
---
+++ src/components/Board/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Box = styled.div`
+  position: absolute;
+  width: 202px;
+  background-color: #000;
+`;
+
+export const Title = styled.div`
+  color: white;
+  margin: 20px 20px 0 20px;
+`;
+
+export const Date = styled.div`
+  margin: 5px 0 24px 20px;
+  color: #939393;
+`;
+
+export const Container = styled.div`
+  position: relative;
+  width: 823px;
+  height: 3000px;
+  top: 200px;
+  left: 50%;
+  margin: 0 0 0 -35%;
+`;
