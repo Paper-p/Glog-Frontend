83017b5dfb4f70fddba3dbbafc24c406db95209a
jongjin <jongjin024@gmail.com>
2022-09-26 20:56:13 +0900

    [STYLE] set signin ui 기본구조

--- src/component/Auth/Signin/index.tsx
+++ src/component/Auth/Signin/index.tsx
@@ -1,7 +1,15 @@
 import Header from "../../Default/Header";
+import * as S from "./style";
 
 const Signin: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <S.SigninWrapper>
+        <S.SigninBox />
+      </S.SigninWrapper>
+    </>
+  );
 };
 
 export default Signin;
---
+++ src/component/Auth/Signin/style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const SigninWrapper = styled.div`
+  display: flex;
+  height: calc(100vh - 170px);
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SigninBox = styled.div`
+  width: 366px;
+  height: 410px;
+  margin-bottom: 170px;
+  background-color: white;
+`;
