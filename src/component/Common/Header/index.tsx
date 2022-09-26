829cedefe63271e5fd3d90238236e14e2969326b
jongjin <jongjin024@gmail.com>
2022-09-26 20:05:34 +0900

    [ADD] emotion + babel

--- src/component/Common/Header/index.tsx
+++ src/component/Common/Header/index.tsx
@@ -1,8 +1,15 @@
 import * as S from "./style";
 import * as I from "../../../Assets/svg/index";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
 const Header: React.FC = () => {
+  const { pathname } = useLocation();
+  const selectHeadItem = (currentPath: string) =>
+    currentPath === pathname && css({ color: "#E0E0E0" });
+
   return (
     <S.HeaderBox>
       <S.Header>
@@ -11,20 +18,26 @@ const Header: React.FC = () => {
             <I.GlogLogo />
           </S.HeaderElements>
           <Link to={"/"}>
-            <S.HeaderElements>홈</S.HeaderElements>
+            <S.HeaderElements css={selectHeadItem("/")}>홈</S.HeaderElements>
           </Link>
           <Link to={"/feed"}>
-            <S.HeaderElements>게시물 작성</S.HeaderElements>
+            <S.HeaderElements css={selectHeadItem("/feed")}>
+              게시물 작성
+            </S.HeaderElements>
           </Link>
         </S.HeaderElementsList>
       </S.Header>
       <S.Header className="right-part">
         <S.HeaderElementsList>
           <Link to={"/signin"}>
-            <S.HeaderElements>로그인</S.HeaderElements>
+            <S.HeaderElements css={selectHeadItem("/signin")}>
+              로그인
+            </S.HeaderElements>
           </Link>
           <Link to={"/signup"}>
-            <S.HeaderElements>회원가입</S.HeaderElements>
+            <S.HeaderElements css={selectHeadItem("/signup")}>
+              회원가입
+            </S.HeaderElements>
           </Link>
         </S.HeaderElementsList>
       </S.Header>
