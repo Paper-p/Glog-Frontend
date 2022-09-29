939e4aae0296beae9ab5397b3d2c4c165944b166
jongjin <jongjin024@gmail.com>
2022-09-30 00:37:37 +0900

    [ADD] 기본적 signin view 구현

--- src/component/Auth/Signin/index.tsx
+++ src/component/Auth/Signin/index.tsx
@@ -1,4 +1,5 @@
 import Header from "../../Default/Header";
+import * as I from "../../../Assets/svg";
 import * as S from "./style";
 
 const Signin: React.FC = () => {
@@ -6,7 +7,15 @@ const Signin: React.FC = () => {
     <>
       <Header />
       <S.SigninWrapper>
-        <S.SigninBox />
+        <S.SigninBox>
+          <S.LogoWrapper>
+            <I.GlogLogo />
+          </S.LogoWrapper>
+          <S.HighlightText>다시 온걸 환영해!</S.HighlightText>
+          <S.InputWrapper className="id"></S.InputWrapper>
+          <S.InputWrapper className="password"></S.InputWrapper>
+          <S.LoginButton>로그인</S.LoginButton>
+        </S.SigninBox>
       </S.SigninWrapper>
     </>
   );
--- src/component/Auth/Signin/style.ts
+++ src/component/Auth/Signin/style.ts
@@ -13,3 +13,56 @@ export const SigninBox = styled.div`
   margin-bottom: 170px;
   background-color: white;
 `;
+
+export const LogoWrapper = styled.div`
+  width: 333px;
+  height: 96px;
+
+  & > svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const HighlightText = styled.div`
+  text-align: center;
+  font-weight: 600;
+  font-size: 20px;
+  padding-top: 20px;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  height: 52px;
+  background: #252727;
+  border-radius: 10px;
+  margin-top: 20px;
+  align-items: center;
+
+  &.password {
+    margin-top: 12px;
+  }
+`;
+
+export const StyledInputBase = styled.input`
+  width: 90%;
+  height: 40px;
+  border: none;
+`;
+
+export const InputID = styled(StyledInputBase)``;
+
+export const InputPassword = styled(StyledInputBase)``;
+
+export const LoginButton = styled.button`
+  width: 366px;
+  height: 60px;
+  background: linear-gradient(95.2deg, #17d56f 45.83%, #119e6d 93.3%);
+  border-radius: 10px;
+  border: none;
+  font-weight: 800;
+  font-size: 18px;
+  color: #1c1c1c;
+  cursor: pointer;
+`;
