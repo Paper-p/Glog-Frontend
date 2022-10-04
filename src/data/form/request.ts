a09df0c86583d80250d4bf133d7a40d2dfdb2708
jongjin <jongjin024@gmail.com>
2022-10-04 15:53:29 +0900

    [ADD] api request form

---
+++ src/data/form/request.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { BASE_URL } from "../../shared/config";
+
+const Request = (p: AxiosRequestConfig, token?: string) => {
+  try {
+    const res = token
+      ? axios({
+          method: p.method,
+          baseURL: BASE_URL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+          headers: {
+            Authorization: "Bearer" + token,
+          },
+        })
+      : axios({
+          method: p.method,
+          baseURL: BASE_URL,
+          url: p.url,
+          data: p.data,
+          withCredentials: true,
+        });
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default Request;
