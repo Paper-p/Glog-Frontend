881e1ed649eb31717b3aed218678449f5caaccee
jongjin <jongjin024@gmail.com>
2022-10-12 10:00:23 +0900

    [REMOVE] 쓰지않는 훅 제거

--- src/component/Auth/Signup/setName/index.tsx
+++ src/component/Auth/Signup/setName/index.tsx
@@ -1,32 +1,20 @@
 import Header from "component/Common/Header";
 import * as S from "./style";
-import * as I from "Assets/svg";
+import Input from "component/Common/Input";
 import { useState } from "react";
-import useInputs from "hooks/useInputs";
 import auth from "data/request/auth";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const SetName: React.FC = () => {
-  const [Focused, setFocused] = useState<boolean>(false);
-  const [, setIsError] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const navigate = useNavigate();
-  const [{ name }, onChange, reset] = useInputs({
-    name: "",
-  });
-
-  const checkUserName = async () => {
-    try {
-      const result: any = await auth.confirmName(name);
-      console.log(result.status);
-      if (result.status === 200) {
-        navigate("/");
-      }
-    } catch (e: any) {
-      setIsError(true);
-    }
-    reset();
-  };
-
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<{ nickname: string }>();
   return (
     <>
       <Header />
@@ -35,25 +23,13 @@ const SetName: React.FC = () => {
           <S.GifBox>
             <S.StyledGif src="/images/Signup.gif" alt="gif" />
           </S.GifBox>
-          <S.InputWrapper>
-            <S.SortInput>
-              <S.StyledSvg>
-                <I.IdAndName />
-              </S.StyledSvg>
-              <S.InputName
-                name="name"
-                placeholder="사용하실 닉네임을 입력해주세요"
-                onFocus={() => {
-                  setFocused(true);
-                }}
-                onChange={onChange}
-                value={name}
-              />
-            </S.SortInput>
-          </S.InputWrapper>
-          <S.SignupButton isFocused={Focused} onClick={checkUserName}>
-            완료
-          </S.SignupButton>
+          <Input
+            sortation={true}
+            placeholder="사용하실 닉네임을 입력해주세요."
+            register={register}
+            isError={isError}
+          />
+          <S.SignupButton>완료</S.SignupButton>
         </S.SignupBox>
       </S.SignupWrapper>
     </>
--- src/component/Auth/Signup/setName/style.ts
+++ src/component/Auth/Signup/setName/style.ts
@@ -1,9 +1,5 @@
 import styled from "@emotion/styled";
 
-type ButtonPropsType = {
-  isFocused: boolean;
-};
-
 export const SignupWrapper = styled.div`
   display: flex;
   height: calc(100vh - 170px);
@@ -25,74 +21,22 @@ export const StyledGif = styled.img`
   height: 140px;
 `;
 
-export const InputWrapper = styled.div`
-  width: 100%;
-  height: 52px;
-  background: #252727;
-  border-radius: 10px;
-  margin-top: 20px;
-  align-items: center;
-`;
-
-export const StyledInputBase = styled.input`
-  width: 90%;
-  height: 40px;
-  border: none;
-  margin: 6px;
-  background: #252727;
-  font-size: 16px;
-  color: #ffffff7a;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-export const SortInput = styled.div`
-  display: flex;
-  text-align: center;
-  margin-left: 10px;
-
-  & > svg {
-    margin-top: 5px;
-  }
-`;
-
-export const StyledSvg = styled.div`
-  margin: auto;
-
-  & > svg {
-    margin-top: 1.5px;
-  }
-`;
-
-export const InputName = styled(StyledInputBase)``;
-
-export const SignupButton = styled.button<ButtonPropsType>`
+export const SignupButton = styled.button`
   width: 366px;
   height: 60px;
-  background: ${(e) => {
-    return e.isFocused
-      ? "linear-gradient(95.2deg, #17D56F 45.83%, #119E6D 93.3%)"
-      : "#505151";
-  }};
+  background: linear-gradient(95.2deg, #17d56f 45.83%, #119e6d 93.3%);
   border-radius: 10px;
   border: none;
   font-weight: 800;
   font-size: 18px;
-  color: ${(e) => {
-    return e.isFocused ? "#1C1C1C" : "#9E9E9E";
-  }};
-  cursor: ${(e) => {
-    return e.isFocused ? "pointer" : "";
-  }};
+  color: #1c1c1c;
+  cursor: pointer;
   margin-top: 20px;
 
   &:hover {
-    ${(e) => {
-      return e.isFocused
-        ? "box-sizing: border-box; background: #f5f5f5; border: 2px solid #17d46f; color: #17d46f;"
-        : "";
-    }};
+    box-sizing: border-box;
+    background: #f5f5f5;
+    border: 2px solid #17d46f;
+    color: #17d46f;
   }
 `;
