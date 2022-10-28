4cbd96bdeb0441e178b6cd20b6e801e7662a358c
jongjin <jongjin024@gmail.com>
2022-10-28 21:16:14 +0900

    [ADD] 이미지 업로드 request

---
+++ src/data/request/image.ts
@@ -0,0 +1,22 @@
+import { REACT_APP_BASE_URL } from "shared/config";
+import AxiosInstance from "util/axiosInstance";
+
+class Auth {
+  uploadImage(thumnailImage: FormData) {
+    try {
+      return AxiosInstance({
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+        url: REACT_APP_BASE_URL + "image",
+        data: {
+          image: FormData,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default new Auth();
