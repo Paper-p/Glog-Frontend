2e7ebe8728aed77d7207f7c9d345bbdcdb0b96b6
jongjin <jongjin024@gmail.com>
2022-11-02 08:35:43 +0900

    [FIX] Button 커스텀화

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,7 +1,35 @@
 import * as S from "./style";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-interface Props extends React.HTMLAttributes<HTMLButtonElement> {}
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  width?: string;
+  background?: string;
+}
 
-export default function Button({ children, ...rest }: Props) {
-  return <S.Button {...rest}>{children}</S.Button>;
+export default function Button({
+  width,
+  background,
+  children,
+  ...rest
+}: Props) {
+  const custom = (data: Props) =>
+    css({ width: data.width }, { background: data.background });
+
+  const data = {
+    width,
+    background,
+  };
+
+  return (
+    <>
+      {data ? (
+        <S.Button css={custom(data)} {...rest}>
+          {children}
+        </S.Button>
+      ) : (
+        <S.Button {...rest}>{children}</S.Button>
+      )}
+    </>
+  );
 }
