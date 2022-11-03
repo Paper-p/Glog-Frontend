3b19d534897d0d0d44863d3764b5f30fb0748a6f
minjekang <kangminje777@gmail.com>
2022-11-03 17:57:33 +0900

    [ADD] 좋아요 아이콘 svg 추가

---
+++ src/Assets/svg/Like.tsx
@@ -0,0 +1,63 @@
+export const Like = () => {
+  return (
+    <svg
+      width="17"
+      height="14"
+      viewBox="0 0 17 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.1029 7.11028C0.941229 5.94912 0.94123 4.06651 2.1029 2.90534C3.26457 1.74418 5.14801 1.74418 6.30967 2.90534L12.6198 9.21275L8.41306 13.4177L2.1029 7.11028Z"
+        fill="#858585"
+      />
+      <path
+        d="M2.1029 7.11028C0.941229 5.94912 0.94123 4.06651 2.1029 2.90534C3.26457 1.74418 5.14801 1.74418 6.30967 2.90534L12.6198 9.21275L8.41306 13.4177L2.1029 7.11028Z"
+        stroke="black"
+      />
+      <g filter="url(#filter0_d_1563_168)">
+        <path
+          d="M10.5585 2.94817C11.7201 1.78701 13.6036 1.78701 14.7653 2.94817V2.94817C15.9269 4.10933 15.9269 5.99195 14.7653 7.15311L8.45509 13.4605L4.24832 9.25558L10.5585 2.94817Z"
+          fill="#D9D9D9"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_1563_168"
+          x="2.24829"
+          y="0.0773926"
+          width="13.3882"
+          height="13.3831"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="-1" dy="-1" />
+          <feGaussianBlur stdDeviation="0.5" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_1563_168"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1563_168"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
--- src/Assets/svg/index.tsx
+++ src/Assets/svg/index.tsx
@@ -2,3 +2,4 @@ export { GlogLogo } from "./GlogLogo";
 export { Glog } from "./Glog";
 export { IdAndName } from "./IdAndName";
 export { Password } from "./Password";
+export { Like } from "./Like";
