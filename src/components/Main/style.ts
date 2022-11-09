7ca974d7d0a7effe50896b96804bdbf61f166d16
jongjin <jongjin024@gmail.com>
2022-11-09 20:06:32 +0900

    [ADD] 임시 category

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -1,1 +1,30 @@
 import styled from "@emotion/styled";
+
+export const CategoryBox = styled.div`
+  width: 72.5%;
+  margin: auto;
+  padding-top: 400px;
+`;
+
+export const Catrgory = styled.div`
+  width: 99px;
+  height: 32px;
+  background: rgba(51, 51, 51, 0.6);
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 16px;
+  text-align: center;
+  line-height: 32px;
+  color: #969696;
+  margin-bottom: 16px;
+`;
+
+export const PostListSection = styled.div`
+  width: 72.5%;
+  gap: 16px 16px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+export const PostWrapper = styled.div``;
