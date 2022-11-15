0997fae7a2567373c9c3cc3cd8476115beb29a5b
jongjin <jongjin024@gmail.com>
2022-11-15 14:21:43 +0900

    [ADD] 댓글 수정

--- src/components/DetailsPost/Comment/index.tsx
+++ src/components/DetailsPost/Comment/index.tsx
@@ -1,16 +1,37 @@
 import { Control, Kebob } from "assets/svg";
+import { loggedAtom } from "atoms";
 import { Button } from "components/Common";
+import comment from "data/request/comment";
+import feed from "data/request/feed";
+import useInputs from "hooks/useInputs";
 import React, { useEffect, useState } from "react";
+import { useQueryClient, useQuery, useMutation } from "react-query";
+import { useParams } from "react-router-dom";
 import TextareaAutosize from "react-textarea-autosize";
+import { useRecoilState } from "recoil";
 import { CommentType } from "types/commentType";
 import * as S from "./style";
 
-interface Props extends CommentType {}
+interface Props extends CommentType {
+  setState: (x: boolean) => void;
+}
 
-function DetailsPostComment({ author, content, createdAt, isMine }: Props) {
+function DetailsPostComment({
+  id,
+  author,
+  content,
+  createdAt,
+  isMine,
+  setState,
+}: Props) {
+  const [{ edit }, onChange] = useInputs({
+    edit: content,
+  });
+  const params = useParams();
   const [isClick, setIsClick] = useState<boolean>(false);
   const [isEdit, setIsEdit] = useState<boolean>(false);
-  const [isRemove, setIsRemove] = useState<boolean>(false);
+  const [logged] = useRecoilState(loggedAtom);
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     if (isClick === false) {
@@ -18,27 +39,68 @@ function DetailsPostComment({ author, content, createdAt, isMine }: Props) {
     }
   }, [isClick]);
 
+  const query = useQuery({
+    queryKey: ["edit"],
+    queryFn: () => reload(),
+  });
+
+  const reload = async () => {
+    try {
+      const res: any = await feed.getDetailsPost(
+        Number(params.postId),
+        logged
+          ? JSON.parse(localStorage.getItem("token") || "{}").accessToken
+          : ""
+      );
+      setState(res.data);
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
+
+  const editComment = async () => {
+    const { res }: any = await comment.editComment(id, edit);
+    return res;
+  };
+
+  const saveComment = useMutation(() => editComment(), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("edit");
+      setIsEdit(false);
+      setIsClick(false);
+    },
+    onError: () => {},
+    onSettled: () => {},
+  });
+
   return (
     <S.DetailsPostCommentLayout>
-      <S.Commnet>
+      <S.Comment>
         <S.UserBox>
-          <S.UserProfile>
-            <img src={author.profileImageUrl} alt="" />
-          </S.UserProfile>
-          <div>
-            <S.UserName>{author.nickname}</S.UserName>
-            {isEdit ? (
-              <S.EditTextarea>
-                <TextareaAutosize placeholder="댓글을 입력해주세요" />
-                <Button>수정</Button>
-              </S.EditTextarea>
-            ) : (
-              <S.UserComment>{content}</S.UserComment>
-            )}
-          </div>
+          {isEdit ? (
+            <S.EditTextarea>
+              <TextareaAutosize
+                placeholder="댓글을 입력해주세요"
+                onChange={onChange}
+                value={edit}
+                name="edit"
+              />
+              <Button onClick={() => saveComment.mutate()}>수정</Button>
+            </S.EditTextarea>
+          ) : (
+            <>
+              <S.UserProfile>
+                <img src={author.profileImageUrl} alt="" />
+              </S.UserProfile>
+              <div>
+                <S.UserName>{author.nickname}</S.UserName>
+                <S.UserComment>{content}</S.UserComment>
+              </div>
+            </>
+          )}
         </S.UserBox>
         <S.CommentBox isClick={isClick}>
-          <S.CreatedAt>2022-10-10</S.CreatedAt>
+          {isEdit ? <></> : <S.CreatedAt>2022-10-10</S.CreatedAt>}
           <S.Icon isClick={isClick}>
             {isMine ? (
               <React.Fragment>
@@ -59,7 +121,7 @@ function DetailsPostComment({ author, content, createdAt, isMine }: Props) {
             )}
           </S.Icon>
         </S.CommentBox>
-      </S.Commnet>
+      </S.Comment>
       <S.Border />
     </S.DetailsPostCommentLayout>
   );
