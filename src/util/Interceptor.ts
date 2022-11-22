864e9039d423f4f143c486d7a3a96c25d0e9a8f2
jongjin <jongjin024@gmail.com>
2022-11-23 08:55:37 +0900

    [FIX] interceptor

--- src/util/Interceptor.ts
+++ src/util/Interceptor.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { getAuth } from "data/url/getUrl";
 import { REACT_APP_BASE_URL } from "shared/config";
 import AxiosInstance from "./AxiosInstance";
@@ -9,6 +9,39 @@ export const instance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+const refresh = async (originalConfig: AxiosRequestConfig) => {
+  try {
+    const res: any = await axios({
+      method: "PATCH",
+      url: getAuth.tokenReissuance(),
+      headers: {
+        RefreshToken: JSON.parse(localStorage.getItem("token") || "{}")
+          .refreshToken,
+      },
+    });
+
+    localStorage.removeItem("token");
+    localStorage.setItem("token", JSON.stringify(res.data));
+    console.log(
+      "new RefreshToken",
+      JSON.parse(JSON.parse(localStorage.getItem("token") || "{}").refreshToken)
+    );
+
+    const expiredAtDate = new Date(
+      JSON.parse(localStorage.getItem("token") || "{}").expiredAt
+    );
+
+    localStorage.setItem(
+      "expiredAt",
+      expiredAtDate.setHours(expiredAtDate.getHours() + 9).toString()
+    );
+
+    return AxiosInstance(originalConfig);
+  } catch (_error: any) {
+    return Promise.reject(_error);
+  }
+};
 
 instance.interceptors.request.use(
   (config: any) => {
@@ -33,33 +66,7 @@ instance.interceptors.response.use(
     if (err.response) {
       if (err.response.status === 401 && !originalConfig._retry) {
         originalConfig._retry = true;
-
-        try {
-          const res: any = await axios({
-            method: "PATCH",
-            url: getAuth.tokenReissuance(),
-            headers: {
-              RefreshToken: JSON.parse(localStorage.getItem("token") || "{}")
-                .refreshToken,
-            },
-          });
-
-          localStorage.removeItem("token");
-          localStorage.setItem("token", JSON.stringify(res.data));
-
-          const expiredAtDate = new Date(
-            JSON.parse(localStorage.getItem("token") || "{}").expiredAt
-          );
-
-          localStorage.setItem(
-            "expiredAt",
-            expiredAtDate.setHours(expiredAtDate.getHours() + 9).toString()
-          );
-
-          return AxiosInstance(originalConfig);
-        } catch (_error: any) {
-          return Promise.reject(_error);
-        }
+        refresh(originalConfig);
       }
     }
 
