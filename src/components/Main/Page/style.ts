1bf8b5d323dcd61ded485cf7a72d35f25840b3a4
jongjin <jongjin024@gmail.com>
2022-11-30 16:19:08 +0900

    [ADD] 메인페이지 카테고리 반응형

--- src/components/Main/Page/style.ts
+++ src/components/Main/Page/style.ts
@@ -4,6 +4,22 @@ export const CategoryBox = styled.div`
   width: 72.5%;
   margin: auto;
   padding: 90px 0 22.5px 0;
+
+  @media screen and (max-width: 1350px) {
+    width: 88.5vw;
+  }
+
+  @media screen and (max-width: 1000px) {
+    width: 93vw;
+  }
+
+  @media screen and (max-width: 850px) {
+    width: 80vw;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 90vw;
+  }
 `;
 
 export const PostListSection = styled.div`
