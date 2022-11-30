ea12064c64505ce110dfe225ec080e9bd946ab4f
jongjin <jongjin024@gmail.com>
2022-11-30 22:36:05 +0900

    [ADD] 반응형 적용

--- src/components/Main/HotPosts/style.ts
+++ src/components/Main/HotPosts/style.ts
@@ -24,4 +24,41 @@ export const CategoryBox = styled.div`
   }
 `;
 
-export const HotPostList = styled.div``;
+export const HotPostList = styled.div`
+  width: 72.5%;
+  gap: 0.83vw 0.83vw;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+
+  @media screen and (max-width: 1350px) {
+    width: 90vw;
+    gap: 1.3vw 0.6vw;
+  }
+
+  @media screen and (max-width: 1000px) {
+    width: 95vw;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 850px) {
+    width: 85vw;
+    gap: 0.83vw 0;
+  }
+
+  @media screen and (max-width: 650px) {
+    width: 80vw;
+    gap: 2vw;
+    overflow: auto;
+    white-space: nowrap;
+    display: flex;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 90vw;
+    gap: 2vw;
+    overflow: auto;
+    white-space: nowrap;
+    display: flex;
+  }
+`;
