07446274b5ab00125166888082b8f967354ef443
minjekang <kangminje777@gmail.com>
2022-12-04 00:05:06 +0900

    [ADD] 마이페이지 카테고리 퍼블리싱

---
+++ src/components/Common/MyCategory/index.tsx
@@ -0,0 +1,5 @@
+import * as S from "./style";
+
+export default function MyCategory({ children }: any) {
+  return <S.MyCategory>{children}</S.MyCategory>;
+}
---
+++ src/components/Common/MyCategory/style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const MyCategory = styled.div`
+  cursor: pointer;
+  background-color: #33333399;
+  width: fit-content;
+  padding: 8px 13px;
+  text-align: center;
+  border-radius: 10px;
+  color: #969696;
+`;
