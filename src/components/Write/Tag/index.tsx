7ac2d3e2d73a9a6cd1984fe17da15f3d96e58f2e
jongjin <jongjin024@gmail.com>
2022-12-04 20:26:48 +0900

    [ADD] 게시물 수정 구현

--- src/components/Write/Tag/index.tsx
+++ src/components/Write/Tag/index.tsx
@@ -11,7 +11,6 @@ interface TagType {
 }
 
 function WriteTag() {
-  const [isRight, setIsRight] = useState<boolean>(false);
   const [onlyNameList, setOnlyNameList] = useState<string[]>([]);
   const [tag, setTag] = useRecoilState(tagAtom);
   const nextId = useRef(1);
@@ -19,7 +18,11 @@ function WriteTag() {
     content: "",
   });
 
+  // useEffect 에 tag 추가 요청을 감지시켜주기 위한 boolean state
+  const [isRight, setIsRight] = useState<boolean>(false);
+
   useEffect(() => {
+    // 요청이왔는지 && 이미 존재하는 값이 아닌지 검토
     if (isRight && !onlyNameList.includes(content)) {
       setTag(
         tag.concat({
@@ -42,10 +45,12 @@ function WriteTag() {
     (e: any) => {
       if (content !== "" && e.key === "Enter" && tag.length + 1 < 6) {
         setOnlyNameList(
+          // useEffect 에서 다음요소의 중복여부 검토를 위해 name을 이용해 중복검사 배열 만들기
           tag.map(({ name }) => {
             return name;
           })
         );
+        // true => tag 추가 요청을 보냄
         setIsRight(true);
       } else if (e.key === "Enter") {
         setNull("content");
