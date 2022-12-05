ac49b14b914c6a0c95e1344b280d23e192988147
jongjin <jongjin024@gmail.com>
2022-12-05 20:22:01 +0900

    [ADD] 로그아웃 기능구현

--- src/components/Modal/LogoutModal/index.tsx
+++ src/components/Modal/LogoutModal/index.tsx
@@ -1,10 +1,21 @@
-import { logoutModalAtom } from "atoms";
+import { loggedAtom, logoutModalAtom } from "atoms";
 import { Button, ModalLayout } from "components/Common";
+import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import TokenService from "util/TokenService";
 import * as S from "./style";
 
 function LogoutModal() {
   const [, setLogoutModal] = useRecoilState(logoutModalAtom);
+  const [, setLogged] = useRecoilState(loggedAtom);
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    setLogoutModal(false);
+    TokenService.removeUser();
+    setLogged(false);
+    navigate("/");
+  };
 
   return (
     <ModalLayout setModal={setLogoutModal}>
@@ -12,7 +23,7 @@ function LogoutModal() {
         <S.TitleText>로그아웃</S.TitleText>
         <S.Text>정말 로그아웃 하시겠어요?</S.Text>
         <S.ButtonBox>
-          <Button>확인</Button>
+          <Button onClick={onLogout}>확인</Button>
           <Button onClick={() => setLogoutModal(false)}>취소</Button>
         </S.ButtonBox>
       </S.LogoutModal>
