6924f8668c93931caa50572b9b744281ae1a9808
jongjin <jongjin024@gmail.com>
2022-12-05 17:56:24 +0900

    [ADD] 기본 이미지 변수

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,2 +1,5 @@
 export const REACT_APP_BASE_URL =
   "http://glog-env.eba-pvh8srk2.ap-northeast-2.elasticbeanstalk.com";
+
+export const DEFAULT_PROFILE_IMAGE =
+  "https://glog-bucket.s3.ap-northeast-2.amazonaws.com/glog-bucket/person-circle.svg";
