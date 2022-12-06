6fdeb6d8c6b7cb3b4e3e3b194ea8cd5f7749ad41
minjekang <kangminje777@gmail.com>
2022-12-06 17:49:25 +0900

    [ADD] 게시물 좋아요 svg

---
+++ src/Assets/svg/PostLike.tsx
@@ -0,0 +1,94 @@
+export const PostLike = () => {
+  <svg
+    width="44"
+    height="34"
+    viewBox="0 0 44 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d_448_14)">
+      <path
+        d="M10.1924 3.12132C11.364 1.94975 13.2635 1.94975 14.435 3.12132L33.5269 22.2132L23.6274 32.1127C22.8464 32.8937 21.5801 32.8937 20.799 32.1127L3.12133 14.435C1.94976 13.2635 1.94976 11.364 3.12133 10.1924L10.1924 3.12132Z"
+        fill="#ACACAC"
+      />
+    </g>
+    <g filter="url(#filter1_d_448_14)">
+      <path
+        d="M10.9 22.2132L29.9919 3.12131C31.1635 1.94974 33.063 1.94974 34.2345 3.12131L41.3056 10.1924C42.4772 11.364 42.4772 13.2634 41.3056 14.435L23.6279 32.1127C22.8469 32.8937 21.5806 32.8937 20.7995 32.1127L10.9 22.2132Z"
+        fill="#D9D9D9"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_448_14"
+        x="0.242676"
+        y="0.242615"
+        width="33.2843"
+        height="32.4559"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="-1" dy="-1" />
+        <feGaussianBlur stdDeviation="0.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_448_14"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_448_14"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter1_d_448_14"
+        x="8.90002"
+        y="0.242615"
+        width="33.2843"
+        height="32.4559"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="-1" dy="-1" />
+        <feGaussianBlur stdDeviation="0.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_448_14"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_448_14"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>;
+};
